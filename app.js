dfe8bece56aca0cbe309a93c8916031cb10fc515
toufa176 <toufaano@gmail.com>
2025-12-17 12:05:59 +0600

    Updated TaskManager

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("Task Manager loaded");
+const taskForm = document.getElementById("taskFrom");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getAnimations("taskList");
+
+console.log(taskForm ,taskInput ,taskList);
