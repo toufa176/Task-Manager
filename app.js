e3d3bcb7111d93685ea69dc2a126a7953d1af2c6
toufa176 <toufaano@gmail.com>
2025-12-17 12:26:07 +0600

    Updated TaskManager

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
 console.log(taskForm ,taskInput ,taskList);
+taskForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    const taskText = taskInput.value;
+    console.log(taskText);
+});
