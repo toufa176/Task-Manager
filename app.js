7cf38d2b170120e6f519a79c73547cc4619adf36
toufa176 <toufaano@gmail.com>
2025-12-18 17:42:47 +0600

    Updated TaskManager

--- app.js
+++ app.js
@@ -13,20 +13,58 @@ taskForm.addEventListener("submit", function (event) {
     if(taskText === ""){
         return;
     }
-    const li = document.createElement("li");
-    li.innerHTML = `
-        <span class="task-text">${taskText}</span>
-        <button class="delete-btn">❌</button>
-        `;
+    tasks.push({
+        text: taskText,
+        completed: false
+    });
+    saveTasks(tasks);
+    renderTasks();
     
-    taskList.appendChild(li);
-
-      taskInput.value ="";
+    taskInput.value = "";
 });
 
- taskList.addEventListener("click", function(event){
-    if (event.target.cllassList.contains("delete-btn")){
-        event.target.parentElement.remove();
-        return;
+  taskList.addEventListener("click", function(event){
+    if (event.target.classList.contains("delete-btn")){
+        const index = event.target.dataset.index;
+        tasks.splice(index,1);
     }
- });
+
+    if (event.target.classList.contains("task-text")){
+        const index = event.target.nextElementSibling.dataset.index;
+        tasks[index].completed = !tasks[index].completed;
+    }
+
+    saveTasks(tasks);
+    renderTasks();
+  })
+
+ function saveTasks(tasks){
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+ }
+
+function loadTasks(){
+    const data = localStorage.getItem("tasks");
+    return data ? JSON.parse(data) :[];
+}
+
+let tasks = loadTasks();
+
+function renderTasks() {
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) =>{
+        const li = document.createElement("li");
+
+        li.innerHTML =`
+        <span class="task-text ${task.completed? "completed": ""}">
+        ${task.text}
+        </span>
+        <button class="delete-btn" data-index=${index}"> ❌</button>
+        `;
+        taskList.appendChild(li);
+
+    });
+
+}
+
+renderTasks();
